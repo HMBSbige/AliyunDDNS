a27f3e68a05211a317bc87f9dd21734925faaf5c
HMBSbige <admin@bige0.com>
2017-11-03 01:26:54 +0800

    增加基本所需的函数

--- AliyunDDNS/AliyunDDNS.cs
+++ AliyunDDNS/AliyunDDNS.cs
@@ -1,37 +1,149 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Aliyun.Acs.Core;
 using Aliyun.Acs.Core.Profile;
 using Aliyun.Acs.Alidns.Model.V20150109;
 
 namespace AliyunDDNS
 {
-    class AliyunDDNS
+    internal class AliyunDDNS
     {
-        static readonly String regionId = @"cn-hangzhou"; //必填固定值，必须为“cn-hanghou”
-        static readonly String accessKeyId = @""; // your accessKey
-        static readonly String accessKeySecret = @"";// your accessSecret
-        static readonly IClientProfile profile = DefaultProfile.GetProfile(regionId, accessKeyId, accessKeySecret);
-        // 若报Can not find endpoint to access异常，请添加以下此行代码
-        // DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Alidns", "alidns.aliyuncs.com");
-        static readonly IAcsClient client = new DefaultAcsClient(profile);
+        private const string regionId = @"cn-hangzhou";
+        private readonly IAcsClient client;
 
-        public static string Test()
+        public AliyunDDNS(string accessKeyId, string accessKeySecret)
         {
-            DescribeDomainsRequest request = new DescribeDomainsRequest();
+            IClientProfile profile = DefaultProfile.GetProfile(regionId, accessKeyId, accessKeySecret);
+            // 若报Can not find endpoint to access异常，请添加以下此行代码
+            // DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Alidns", "alidns.aliyuncs.com");
+            client = new DefaultAcsClient(profile);
+        }
 
+        /// <summary>
+        /// 获取某域名的解析记录列表
+        /// </summary>
+        /// <param name="Domain"></param>
+        /// <returns></returns>
+        public List<DescribeDomainRecordsResponse.Record> GetDomainRecords(string Domain)
+        {
+            var request = new DescribeDomainRecordsRequest()
+            {
+                DomainName = Domain,
+                PageNumber = 1,
+                PageSize = 500
+            };
             try
             {
-                DescribeDomainsResponse response = client.GetAcsResponse(request);
-
-                return response.Domains[0].DomainName;
+                var response = client.GetAcsResponse(request);
+                return response.DomainRecords;
             }
             catch
             {
-                return @"Error";
+                return new List<DescribeDomainRecordsResponse.Record>();
             }
         }
 
-        private static long longnullable2long(long? z)
+        /// <summary>
+        /// 获取解析的记录值
+        /// </summary>
+        /// <param name="RecordID"></param>
+        /// <returns></returns>
+        public string GetDomainRecordValue(string RecordID)
+        {
+            var request = new DescribeDomainRecordInfoRequest()
+            {
+                RecordId= RecordID
+            };
+            try
+            {
+                var response = client.GetAcsResponse(request);
+                return response.Value;
+            }
+            catch
+            {
+                return @"";
+            }
+        }
+
+        /// <summary>
+        /// 获取某子域名A记录的解析值
+        /// </summary>
+        /// <param name="SubDomain"></param>
+        /// <returns></returns>
+        public string GetSubDomainARecord(string SubDomain)
+        {
+            var request = new DescribeSubDomainRecordsRequest()
+            {
+                SubDomain= SubDomain,
+                PageNumber = 1,
+                PageSize = 1,
+                Type = @"A"
+
+            };
+            try
+            {
+                var response = client.GetAcsResponse(request);
+                return response.DomainRecords[0].Value;
+            }
+            catch
+            {
+                return @"";
+            }
+        }
+
+        /// <summary>
+        /// 获取某子域名A记录的RecordId
+        /// </summary>
+        /// <param name="SubDomain"></param>
+        /// <returns></returns>
+        public string GetSubDomainRecordId(string SubDomain)
+        {
+            var request = new DescribeSubDomainRecordsRequest()
+            {
+                SubDomain = SubDomain,
+                PageNumber = 1,
+                PageSize = 1,
+                Type = @"A"
+
+            };
+            try
+            {
+                var response = client.GetAcsResponse(request);
+                return response.DomainRecords[0].RecordId;
+            }
+            catch
+            {
+                return @"";
+            }
+        }
+
+        /// <summary>
+        /// 修改A解析记录
+        /// </summary>
+        /// <param name="RR">主机记录，如果要解析@.exmaple.com，主机记录要填写"@”，而不是空</param>
+        /// <param name="Domain">域名</param>
+        /// <param name="Value">记录值</param>
+        /// <returns></returns>
+        public string UpdateDomainRecord(string RR, string Domain,string Value)
+        {
+            var request = new UpdateDomainRecordRequest()
+            {
+                RecordId = GetSubDomainRecordId(RR+@"."+ Domain),
+                RR =RR,
+                Type = @"A",
+                Value= Value
+            };
+            try
+            {
+                var response = client.GetAcsResponse(request);
+                return response.RecordId;
+            }
+            catch
+            {
+                return @"";
+            }
+        }
+        
+        public static long Longnullable2long(long? z)
         {
             return z.HasValue ? z.GetValueOrDefault() : long.MinValue;
 
--- AliyunDDNS/Program.cs
+++ AliyunDDNS/Program.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 
-using Aliyun.Acs.Alidns.Model.V20150109;
-
 namespace AliyunDDNS
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(@"Hello World!");
+
+            var t1=new AliyunDDNS(@"", @"");
+
+            
+            const string Domain = @"bige0.com";
+            const string RR = @"school";
+            const string Value = @"8.8.8.8";
+
+            const string SubDomain = RR+@"."+ Domain;
+            var lastValue = t1.GetSubDomainARecord(SubDomain);
+            var lastRecordId = t1.GetSubDomainRecordId(SubDomain);
+
+            if (lastValue != Value)
+            {
+                if (t1.UpdateDomainRecord(@"school", @"bige0.com", Value) == lastRecordId &&
+                    t1.GetSubDomainARecord(SubDomain) == Value)
+                {
+                    Console.WriteLine(@"IP 地址更改成功");
+                }
+                else
+                {
+                    Console.WriteLine(@"IP 地址更改失败");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(@"IP 地址未改变");
+            }
+            Console.WriteLine(@"Goodbye!");
         }
     }
 }
